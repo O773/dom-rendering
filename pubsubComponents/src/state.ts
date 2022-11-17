e761b2cf844f3d6b29e53298084210d053b546b4
Olle Johannesson <olle.johannesson@objectbay.com>
2022-11-17 20:52:40 +0100

    clear subscribers after notifying
    the subscribe method will be called again for the newly rendered component

--- pubsubComponents/src/state.ts
+++ pubsubComponents/src/state.ts
@@ -1,100 +1,61 @@
 import * as objectpath from 'object-path'
+import { removeInitialDollar } from "./util";
+
 let state = {}
 
 const observers = new Map<any, Set<Function>>()
 
-const removeInitialDollar = (s: string) => s.replace(/^\$/, '')
-
-const subscribe = (val: any, cb: (newValue: any) => void) => {
-  val = removeInitialDollar(val)
-  if(!observers.has(val)) {
-    observers.set(val, new Set([cb]))
+const subscribe = (jsonPath: any, onNotify: (newValue: any) => void) => {
+  jsonPath = removeInitialDollar(jsonPath)
+  if(!observers.has(jsonPath)) {
+    observers.set(jsonPath, new Set([onNotify]))
   } else {
-    observers.get(val)?.add(cb)
+    observers.get(jsonPath)?.add(onNotify)
   }
 }
 
-const get = (val: any) => {
-  val = removeInitialDollar(val)
-  return objectpath.get(state, val)
+const get = (jsonPath: string) => {
+  jsonPath = removeInitialDollar(jsonPath)
+  return objectpath.get(state, jsonPath)
 }
 
-const set = (val: string, newValue: any) => {
-  val = removeInitialDollar(val);
-  objectpath.set(state, val, newValue);
-  [...observers.get(val)?.values() || []].forEach(cb => cb(newValue))
+const set = (jsonPath: string, newValue: any) => {
+  if (get(jsonPath) === newValue) {
+    return
+  }
+  jsonPath = removeInitialDollar(jsonPath);
+  objectpath.set(state, jsonPath, newValue);
+  const callbacks = [...observers.get(jsonPath)?.values() || []]
+  observers.set(jsonPath, new Set())
+  callbacks.forEach(cb => cb(newValue))
 }
 
-const init = (val: Object | string | null) => {
-  if (!val) {
+const init = (value: Object | string | null) => {
+  if (!value) {
     return
   }
 
-  switch (typeof val) {
-    case 'string': {
-      state = { ...JSON.parse(val) }
-      break
+  const doSet = (val: Object | string | null) => {
+    switch (typeof val) {
+      case 'string': {
+        state = { ...JSON.parse(val) }
+        break
+      }
+      case 'object': {
+        state = { ...val }
+        break
+      }
+      default: {
+        return
+      }
     }
-    case 'object': {
-      state = { ...val }
-      break
-    }
-    default: {
-      return
-    }
+  }
+
+  if (value instanceof Promise) {
+      value.then(doSet)
+  } else {
+      doSet(value)
   }
 }
 
 export { subscribe, get, set, init }
-
-
-
-
-
-// const isProxy = Symbol("isProxy")
-
-// export const subscribe = (path: string, onChange: Function, object = state) => {
-//   if (!subscribers.has(path)) {
-//     subscribers.set(path, new Set())
-//   }
-
-//   subscribers.get(path)?.add(onChange)
-
-//   const value = objectpath.get(object, path)
-
-//   return value
-// }
-
-// const handler = {
-//   get(target, key) {
-//     if (key === isProxy) {
-//       return true
-//     }
-
-//     const prop = target[key]
-
-//     // return if property not defined on target
-//     if (!(key in target)) {
-//       return
-//     }
-
-//     // set value as proxy if object
-//     if (!prop[isProxy] && typeof prop === 'object')
-//       target[key] = new Proxy(prop, handler)
-
-//     return target[key]
-//   },
-//   set(target, key, value, receiver) {
-    
-//     console.log('Setting', target, `.${key} to equal`, value)
-
-//     if(!subscribers.has(receiver)) {
-//       subscribers.set(receiver, new Set())
-//     } else {
-//       [...subscribers.get(receiver)?.values() || []].forEach(cb => cb(value))
-//     }
-
-//     target[key] = value
-//     return true
-//   }
-// }
--- pubsubComponents/src/util.ts
+++ pubsubComponents/src/util.ts
@@ -2,6 +2,8 @@ export const kebabize = (str) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) 
 
 export const objToCss = (obj: { [key: string]: string | number }) => Object.entries(obj).reduce((str, [key, value]) => str.concat(`${kebabize(key)}:${value.toString()};`), '')
 
+export const removeInitialDollar = (s: string) => s.replace(/^\$/, '')
+
 export const isNullish = (arg: any | null | undefined) => arg === null || arg === undefined
 
 export const thunk = (arg: any, val?: any) => typeof arg === 'function' ? arg(val) : arg
